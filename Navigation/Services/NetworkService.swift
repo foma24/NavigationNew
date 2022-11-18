29bb5bd38503940c53ca6413d38f03c3b0c26751
Denis Fomichev <fomichev.ds88@gmail.com>
2022-11-18 18:31:32 -0500

    HW_iOS_DT1.2

--- Navigation/Services/NetworkService.swift
+++ Navigation/Services/NetworkService.swift
@@ -3,7 +3,7 @@ import Foundation
 struct NetworkService {
     //MARK: - request
     static func request(for configuration: AppConfiguration) {
-        guard let url: URL = configuration.url else { return }
+        guard let url: URL = configuration.url else { preconditionFailure() }
         let urlTask = URLSession.shared.dataTask(with: url) { data, response, error in
             
             if let data = data {
@@ -18,6 +18,57 @@ struct NetworkService {
             print("Error: \(String(describing: error))\nError discription: \(String(describing: error?.localizedDescription))")
         }
         urlTask.resume()
+    }
+    
+    static func request(id: Int, completion: @escaping (String) -> Void) {
+        guard let url: URL = URL(string: ("https://jsonplaceholder.typicode.com/todos/" + String(id))) else { preconditionFailure() }
+        let urlTask = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                do{
+                    let object = try JSONSerialization.jsonObject(with: data, options: [])
+                    if let idInfo = object as? [String : Any] {
+                        guard let title = idInfo["title"] as? String else { return }
+                        completion(title)
+                    }
+                } catch let error {
+                    print("Error discription: \(error)")
+                }
+            }
+        }
+        urlTask.resume()
+    }
+    
+    static func request(completion: @escaping (PlanetModel) -> Void) {
+        guard let url: URL = URL(string: "https://swapi.dev/api/planets/1") else { preconditionFailure() }
+        let urlTask = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                do {
+                    let planet = try JSONDecoder().decode(PlanetModel.self, from: data)
+                    completion(planet)
+                } catch let error {
+                    print("Error discription: \(error)")
+                }
+            }
+        }
+        urlTask.resume()
+    }
+    
+    static func requestResidents(completion: @escaping (Residents) -> Void) {
+        NetworkService.request { planet in
+            planet.residentsURL?.forEach { residentURL in
+                let urlTask = URLSession.shared.dataTask(with: residentURL) { data, response, error in
+                    if let data = data {
+                        do {
+                            let resident = try JSONDecoder().decode(Residents.self, from: data)
+                            completion(resident)
+                        } catch let error {
+                            print("Error discription: \(error)")
+                        }
+                    }
+                }
+                urlTask.resume()
+            }
+        }
     }
 }
 
--- Navigation/ViewControllers/InfoViewController.swift
+++ Navigation/ViewControllers/InfoViewController.swift
@@ -2,7 +2,53 @@ import UIKit
 
 class InfoViewController: UIViewController {
     
-    var alertButton = UIButton()
+    var residents: [Residents] = []
+    
+    private lazy var alertButton: UIButton = {
+        let alertButton = UIButton()
+        alertButton.toAutoLayout()
+        alertButton.layer.cornerRadius = 10
+        alertButton.clipsToBounds = true
+        alertButton.backgroundColor = .red
+        alertButton.setTitle("Alert", for: .normal)
+        alertButton.setTitleColor(.white, for: .normal)
+        alertButton.addTarget(self, action: #selector(alertButtonTapped), for: .touchUpInside)
+        
+        return alertButton
+    }()
+    
+    private let urlLabel: UILabel = {
+        let urlLabel = UILabel()
+        urlLabel.toAutoLayout()
+        NetworkService.request(id: 2) { title in
+            DispatchQueue.main.async {
+                urlLabel.text = "Title from URL: \(title)"
+            }
+        }
+        
+        return urlLabel
+    }()
+    
+    private let planetLabel: UILabel = {
+        let planetLabel = UILabel()
+        planetLabel.toAutoLayout()
+        NetworkService.request { planet in
+            DispatchQueue.main.async {
+                planetLabel.text = "Tatooine orbital period: \(planet.orbitalPeriod)"
+            }
+        }
+        return planetLabel
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.toAutoLayout()
+        tableView.layer.cornerRadius = 10
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        return tableView
+    }()
     
     //MARK: - viewDidLoad
     override func viewDidLoad() {
@@ -10,19 +56,47 @@ class InfoViewController: UIViewController {
         
         view.backgroundColor = .lightGray
         
-        alertButtonConfig() 
+        setupSubviews()
+        setupSubviewsLayout()
+        updateTableView()
     }
     
-    //MARK: - alert button config
-    func alertButtonConfig() {
-        alertButton = UIButton(frame: CGRect(x: view.frame.width/2-100, y: view.frame.height/2-25, width: 200, height: 50))
-        alertButton.layer.cornerRadius = 12
-        alertButton.backgroundColor = .red
-        alertButton.setTitle("Alert", for: .normal)
-        
-        alertButton.addTarget(self, action: #selector(alertButtonTapped), for: .touchUpInside)
-        
+    //MARK: - setupSubviews
+    private func setupSubviews(){
         view.addSubview(alertButton)
+        view.addSubview(urlLabel)
+        view.addSubview(planetLabel)
+        view.addSubview(tableView)
+    }
+    
+    private func setupSubviewsLayout() {
+        NSLayoutConstraint.activate([
+            alertButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            alertButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            alertButton.widthAnchor.constraint(equalToConstant: 200),
+            alertButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            urlLabel.topAnchor.constraint(equalTo: alertButton.bottomAnchor, constant: 16),
+            urlLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            planetLabel.topAnchor.constraint(equalTo: urlLabel.bottomAnchor, constant: 16),
+            planetLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            planetLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            tableView.topAnchor.constraint(equalTo: planetLabel.bottomAnchor, constant: 16),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
+        ])
+    }
+    
+    private func updateTableView() {
+        NetworkService.requestResidents { [weak self] resident in
+            DispatchQueue.main.async {
+                self?.residents.append(resident)
+                self?.tableView.reloadData()
+            }
+        }
     }
     
     //MARK: - alertButtonTapped
@@ -38,5 +112,46 @@ class InfoViewController: UIViewController {
         alert.addAction(secondAlertAction)
         
         self.present(alert, animated: true, completion: nil)
+    }
+}
+
+extension InfoViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return residents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "nil")
+        var content = cell.defaultContentConfiguration()
+        content.text = residents[indexPath.row].name
+        cell.contentConfiguration = content
+
+        return cell
+    }
+}
+
+extension InfoViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        UITableView.automaticDimension
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return UIView(frame: .zero)
+    }
+
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView(frame: .zero)
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
